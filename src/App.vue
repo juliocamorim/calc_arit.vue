c91f278dc870809805535ee6a7d2602d4e22a784
Júlio Cézar <jc.amorim.n01@gmail.com>
2024-05-16 21:45:33 -0300

    criando a calculadora.vue

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import Calculadora from '../src/components/Calculadora.vue';
+
+
+// exportação do objeto
+export default {
+  name: 'App',
+  components: {
+    Calculadora,
+  },
+};
+</script>
+
+<template>
+  <div class="container">
+    <Calculadora />
+  </div>
+</template>
+
+<style></style>
---
+++ src/components/Calculadora.vue
@@ -0,0 +1,78 @@
+<script setup>
+import { reactive } from 'vue';
+
+const estado = reactive ({
+    campoUm: '',
+    campoDois: '',
+    operacoes: {
+    adicao: (a, b) => a + b,
+        subtracao: (a, b) => a - b,
+       multiplicacao: (a, b) => a * b,
+        divisao: (a, b) => a / b,
+    },
+    resultado: 0,
+});
+//definido objeto react
+
+const calculaResultado = () => {
+    const { campoUm, campoDois, operacao } = estado;
+    estado.resultado = estado.operacoes[operacao](parseFloat(campoUm), parseFloat(campoDois));
+};
+</script>
+
+<template>
+<div class="container">
+<h1>Calculadora Aritmética</h1>
+
+    <input type="number" v-model="estado.campoUm" @input="calculaResultado">
+    <input type="number" v-model="estado.campoDois" @input="calculaResultado">
+
+    <h2>Escolha a operação desejada</h2>
+    <select v-model="estado.operacao" @change="calculaResultado">
+        <option value="adicao">Adição</option>
+        <option value="subtracao">Subtração</option>
+        <option value="multiplicacao">Multiplicação</option>
+        <option value="divisao">Divisão</option>
+    </select>
+
+    <h2>Resultado: {{ estado.resultado }}</h2>
+</div>
+</template>
+
+<style scoped>
+
+* {
+    padding: 0;
+    margin: 0 auto;
+}
+.container {
+    text-align: center;
+}
+
+h1 {
+    font-size: 36px;
+    margin: 32px;
+}
+
+h2 {
+    font-size: 26px;
+}
+
+h2,
+select
+    {
+    margin-bottom: 16px;
+}
+
+input,
+select {
+    width: 120px;
+    height: 24px;
+    text-align: center;
+}
+
+input {
+    margin-bottom: 24px;
+    display: flex;
+}
+</style>
